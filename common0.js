de3c4de3adedb5b8a3906efe897345fd33ce8241
triple-underscore <triple-underscore@users.noreply.github.com>
2017-02-28 13:47:46 +0900

    編集（和訳追加に伴うリンク更新

--- common0.js
+++ common0.js
@@ -857,6 +857,7 @@ HTMLdom:HTML-dom-ja.html\n\
 HTMLforms:HTML-forms-ja.html\n\
 HTMLindex:HTML-index-ja.html\n\
 HTMLlinks:HTML-links-ja.html\n\
+HTMLnavigator:HTML-navigator-ja.html\n\
 HTMLinteraction:HTML-interaction-ja.html\n\
 HTMLrendering:HTML-rendering-ja.html\n\
 HTMLselectors:selectors-html-ja.html\n\
