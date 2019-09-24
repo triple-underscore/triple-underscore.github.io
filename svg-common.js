0439db293f2292b4a20483f61a7a4a5fd3c0ac05
triple-underscore <triple-underscore@users.noreply.github.com>
2019-09-24 13:58:53 +0900

    [SVG2] https://github.com/w3c/svgwg/commit/f4e1c91447fd725f27165eb9d387e2a425c9e89a Remove onfocusin and onfocusout event attributes.

--- svg-common.js
+++ svg-common.js
@@ -640,7 +640,6 @@ t.dasharray:~SVGpainting#DataTypeDasharray
 接触判定:~SVGinteract#TermHitTesting
 ~focus可能:~SVGinteract#TermFocusable
 ~event属性:~SVGinteract#TermEventAttribute
-~graphicな~event属性:~SVGinteract#EventAttributes
 
 ~prop:~SVGstyling#TermProperty
 呈示~属性:~SVGstyling#TermPresentationAttribute
@@ -664,7 +663,6 @@ t.dasharray:~SVGpainting#DataTypeDasharray
 加法的でない:~CSSVAL#not-additive
 無視する:~CSSSYN#css-ignored
 既定の~sizing~algo:~CSSIMAGE#default-sizing-algorithm
-	~CSSIMAGE#default-sizing
 大域~event属性:~WAPI#globaleventhandlers
 文書~要素~event属性:~WAPI#documentandelementeventhandlers
 ~CORS設定群~属性:~HTMLurl#cors-settings-attribute
