5a237bc99d9da56a06d41d2354d425deb0c1686f
triple-underscore <triple-underscore@users.noreply.github.com>
2020-01-16 06:41:02 +0900

    [infra] https://github.com/whatwg/infra/commit/6fbd1fad4711528a43558e610ab460aeb3d331d9 Define fingerprinting vector
    • HTML 仕様の用語（ fingerprinting vector ）も Infra を参照するよう先取り的に変更。
    • それに伴い、一部の § を（より特定的な）他所（ Communication 章）へ移動。

--- common0.js
+++ common0.js
@@ -1025,7 +1025,7 @@ SYMBOL_DEF_REF:<a href="index.html#common-algo-symbols">アルゴリズムに共
 ABSTRACT:<h2 title="Abstract">要約</h2>
 STATUSofTHIS:<h2 title="Status of this Document">この文書の位置付け</h2>
 INFORMATIVE:<p><em>この節は規範的ではない。</em><span lang="en">This section is non-normative.</span></p>
-FINGERPRINTING:<a class="fingerprinting" href="HTML-infrastructure-ja.html#fingerprinting-vector"></a>
+FINGERPRINTING:<a class="fingerprinting" href="infra-ja.html#tracking-vector" title="これは利用者の追跡に利用され得る。／There is a tracking vector here."></a>
 CSSisaLANG:<p><a href="css-snapshot-ja.html#css-is-a-lang">CSS とは…</a></p>
 TR:https://www.w3.org/TR
 IETF:https://tools.ietf.org/html
