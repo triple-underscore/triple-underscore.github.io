c86fead7999cad3f7e62e2a636b3016a6f68d40b
triple-underscore <triple-underscore@users.noreply.github.com>
2021-12-23 15:11:11 +0900

    編集（和訳リンクデータ

--- common0.js
+++ common0.js
@@ -1150,6 +1150,7 @@ CSSANIM:css-animations-ja.html
 CSSBG:css-backgrounds-ja.html
 CSSBREAK:css-break-ja.html
 CSSCOLOR:css-color-ja.html
+CSSCOLOR5:css-color5-ja.html
 CSSCOLORADJUST:css-color-adjust-ja.html
 CSSCOND:css-conditional-ja.html
 CSSCONTENT:css-content-ja.html
