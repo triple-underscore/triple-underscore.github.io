9c362749a1ca22fd754ccfba05a04c4835522f5c
triple-underscore <triple-underscore@users.noreply.github.com>
2023-09-23 02:48:06 +0900

    [css-shapes-2] 新規和訳： CSS Shapes Module Level 2 https://drafts.csswg.org/css-shapes-2/

--- common1.js
+++ common1.js
@@ -1676,6 +1676,7 @@ CSSPOSITION3=副       ~/css-position-ja.html
 CSSPOSITION4=副       ~/css-position4-ja.html
 CSSRUBY1=副           ~/css-ruby-ja.html
 CSSSHAPES1=副         ~/css-shapes-ja.html
+CSSSHAPES2=副         ~/css-shapes2-ja.html
 CSSSPEECH1=副         ~/css-speech-ja.html
 CSSSIZING3=副         ~/css-sizing-ja.html
 CSSOM1=副             ~/cssom-ja.html
